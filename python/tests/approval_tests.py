a86aedc62e678eebe0ebdf68b4ee10cf5c1a7e1a
simonnaish <snaish@gmail.com>
2025-03-13 18:34:33 +0000

    Set up pytest approval tests that match the TextTest
    This is for convenience as much as anything else

---
+++ python/tests/approval_tests.py
@@ -0,0 +1,17 @@
+from approvaltests import verify
+from approvaltests.reporters import PythonNativeReporter
+
+from ..gilded_rose.manager import GildedRose
+
+def tests_e2e_approval(all_items):
+    reporter = PythonNativeReporter()
+    gilded_rose = GildedRose(all_items)
+    output = ["OMGHAI!"]
+    for day in range(31):
+        output.append(f"-------- day {day} --------")
+        output.append("name, sellIn, quality")
+        for item in all_items:
+            output.append(str(item))
+        output.append("")
+        gilded_rose.update_quality()
+    verify("\n".join(output), reporter=reporter)
