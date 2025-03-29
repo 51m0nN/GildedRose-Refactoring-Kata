0a6bc36f318bb0c6dcc4703b0d32a8da81a84eae
simonnaish <snaish@gmail.com>
2025-03-29 23:01:20 +0000

    Refactored updaters for maximum legibility and clarity

--- python/gilded_rose/strategies/updaters.py
+++ python/gilded_rose/strategies/updaters.py
@@ -1,12 +1,12 @@
 from ..stock import Item
 
 
-def _decrease_item_quality(item: Item, amount: int = 1) -> None:
+def _decrease_item_quality(item: Item, amount: int = 1, min_quality=0) -> None:
     """Item quality is reduced by amount, but can never drop below 0"""
-    item.quality = max(0, item.quality - amount)
+    item.quality = max(min_quality, item.quality - amount)
 
 
-def _increase_item_quality(item: Item, amount: int = 1, max_quality: int=50) -> None:
+def _increase_item_quality(item: Item, amount: int = 1, max_quality: int = 50) -> None:
     """Item quality is increased by amount, but can never be greater than max_amount"""
     item.quality = min(max_quality, item.quality + amount)
 
@@ -24,10 +24,11 @@ def update_brie_item(item: Item) -> None:
     Brie can never have better than 50 quality
     """
     item.sell_in = item.sell_in - 1
-    _increase_item_quality(item)
 
     if item.sell_in < 0:
-        _increase_item_quality(item)
+        _increase_item_quality(item, amount=2)
+    else:
+        _increase_item_quality(item, amount=1)
 
 
 def update_backstage_pass_item(item: Item) -> None:
@@ -38,14 +39,15 @@ def update_backstage_pass_item(item: Item) -> None:
     Quality goes to 0 after the gig
     """
     item.sell_in = item.sell_in - 1
+
     if item.sell_in < 0:
         item.quality = 0
-    else:
-        _increase_item_quality(item)
-        if item.sell_in < 10:
-            _increase_item_quality(item)
-        if item.sell_in < 5:
-            _increase_item_quality(item)
+    elif item.sell_in < 5:
+        _increase_item_quality(item, amount=3)
+    elif item.sell_in < 10:
+        _increase_item_quality(item, amount=2)
+    elif item.sell_in >= 10:
+        _increase_item_quality(item, amount=1)
 
 
 def update_standard_item(item: Item) -> None:
@@ -53,14 +55,18 @@ def update_standard_item(item: Item) -> None:
     Once its past its sell by date it quality goes down by 2 each time
     """
     item.sell_in = item.sell_in - 1
-    _decrease_item_quality(item)
+
     if item.sell_in < 0:
-        _decrease_item_quality(item)
+        _decrease_item_quality(item, amount=2)
+    else:
+        _decrease_item_quality(item, amount=1)
 
 
 def update_conjured_item(item: Item) -> None:
     """Quality declines at twice the rate of a normal item"""
     item.sell_in = item.sell_in - 1
-    _decrease_item_quality(item, 2)
+
     if item.sell_in < 0:
-        _decrease_item_quality(item, 2)
+        _decrease_item_quality(item, amount=4)
+    else:
+        _decrease_item_quality(item, amount=2)
